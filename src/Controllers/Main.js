71eb8d6c79bc2bb8168514a60099c60092f5df4f
Olexandr Marushchak <olex.marushchak@gmail.com>
2020-01-08 00:09:17 +0200

    classes used + misc

--- src/Controllers/Main.js
+++ src/Controllers/Main.js
@@ -1,22 +1,22 @@
 const PIXI = require("pixi.js");
 const MainView = require("../Views/MainView");
 const MainModel = require("../Models/MainModel");
-const Util = require("../Utils/Util");
 
-function Main() {
-	const app = new PIXI.Application({ backgroundColor: 0x0000FF });
-	const container = document.getElementById("canvas");
-	container.appendChild(app.view);
+class Main {
+	constructor() {
+		const app = new PIXI.Application({ backgroundColor: 0x0000FF });
+		const container = document.getElementById("canvas");
+		container.appendChild(app.view);
+		
+		this.mainModel = new MainModel(app);
+		
+		this.mainView = new MainView(app, this.mainModel);
+		this.mainView.graphics.on("pointerdown", this.onShapeClick, this);
+	}
 	
-	this.mainModel = new MainModel();
-	
-	this.mainView = new MainView(app, this.mainModel);
-	this.mainView.graphics.on("pointerdown", this.onShapeClick, this);
-}
-Util.CreateClass(Main);
-
-Main.prototype.onShapeClick = function(e){
-	this.mainModel.setRandomSize();
+	onShapeClick(e) {
+		this.mainModel.setRandomParameters();
+	}
 }
 
 new Main();
--- src/Models/MainModel.js
+++ src/Models/MainModel.js
@@ -1,23 +1,30 @@
 const PIXI = require("pixi.js");
-const Util = require("../Utils/Util");
 
-function MainModel() {
-	PIXI.utils.EventEmitter.call(this);
+class MainModel extends PIXI.utils.EventEmitter {
+	constructor(app) {
+		super();
+		
+		this.app = app;
+		
+		this.boxWidth = 100;
+		this.boxHeight = 100;
+		this.boxRotation = 0.01;
+		this.boxFillColor = "0xFF0000";
+		this.boxX = 200;
+		this.boxY = 200;
+	}
 	
-	this.boxWidth = 100;
-	this.boxHeight = 100;
-	this.boxRotation = 0.01;
-	this.boxFillColor = "FF0000";
-	this.boxX = 200;
-	this.boxY = 200;
-}
-Util.CreateClass(MainModel, PIXI.utils.EventEmitter);
-
-MainModel.prototype.setRandomSize = function() {
-	this.boxWidth = (Math.random() * 1 + 0.5) * 100;
-	this.boxHeight = (Math.random() * 1 + 0.5) * 100;
-	
-	this.emit("CHANGED");
+	setRandomParameters() {
+		this.boxWidth = (Math.random() * 1 + 0.5) * 100;
+		this.boxHeight = (Math.random() * 1 + 0.5) * 100;
+		
+		this.boxFillColor = Math.floor(Math.random() * 16777215).toString(16);
+		
+		this.boxX = this.app.renderer.width * Math.random();
+		this.boxY = this.app.renderer.height * Math.random();
+		
+		this.emit("CHANGED");
+	}
 }
 
 module.exports = MainModel;
